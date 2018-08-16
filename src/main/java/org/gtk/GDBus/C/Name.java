cdd1ec36077441da7fc52f363be47bf2d6d9c652
David M <maniac@case-of.org>
2018-08-16 09:48:07 +0200

    Added samples for using dbus-java

---
+++ src/main/java/org/gtk/GDBus/C/Name.java
@@ -0,0 +1,8 @@
+package org.gtk.GDBus.C;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Name
+{
+   String value();
+}
