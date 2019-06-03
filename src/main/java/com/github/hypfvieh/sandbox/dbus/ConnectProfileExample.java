915dd1b96a979414ce9595828a893a964d37f972
David M <maniac@case-of.org>
2019-06-03 08:58:35 +0200

    Updated dependencies; Moved classes to correct package

---
+++ src/main/java/com/github/hypfvieh/sandbox/dbus/ConnectProfileExample.java
@@ -0,0 +1,71 @@
+package com.github.hypfvieh.sandbox.dbus;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bluez.Device1;
+import org.bluez.Profile1;
+import org.bluez.ProfileManager1;
+import org.bluez.exceptions.BluezCanceledException;
+import org.bluez.exceptions.BluezRejectedException;
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.dbus.connections.impl.DBusConnection;
+import org.freedesktop.dbus.connections.impl.DBusConnection.DBusBusType;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.dbus.types.Variant;
+
+public class ConnectProfileExample {
+    public static void main(String[] args) throws DBusException, InterruptedException, IOException {
+        // open connection to bluez on SYSTEM Bus
+        DBusConnection connection = DBusConnection.getConnection(DBusBusType.SYSTEM);
+        ProfileImpl object = new ProfileImpl();
+        connection.exportObject("/", object);
+        
+        ProfileManager1 profileManager = connection.getRemoteObject("org.bluez", "/org/bluez", ProfileManager1.class);
+        
+        Map<String, Variant<?>> props = new HashMap<String, Variant<?>>();
+        profileManager.RegisterProfile( new DBusPath("/"), "00001101-0000-1000-8000-00805F9B34FB", props);
+        
+        Device1 device= connection.getRemoteObject("org.bluez", "/org/bluez/hci0/dev_04_4F_4C_FA_04_E7", Device1.class);
+        device.ConnectProfile("00001101-0000-1000-8000-00805F9B34FB");
+        
+        System.out.println("sleeping");
+        
+        Thread.sleep(60000L);
+        
+        System.out.println("shutting down");
+        connection.close();
+    }
+    
+    public static class ProfileImpl implements Profile1 {
+
+        @Override
+        public boolean isRemote() {
+            return false;
+        }
+
+        @Override
+        public String getObjectPath() {
+            return "/";
+        }
+
+        @Override
+        public void Release() {
+            System.err.println("Release called");
+        }
+
+        @Override
+        public void NewConnection(DBusPath _device, FileDescriptor _fd, Map<String, Variant<?>> _fd_properties)
+                throws BluezRejectedException, BluezCanceledException {
+            System.err.println("New connection: FD=" + _fd + ", Path=" + _device + ", Props=" + _fd_properties);
+        }
+
+        @Override
+        public void RequestDisconnection(DBusPath _device) throws BluezRejectedException, BluezCanceledException {
+            System.err.println("Disconnect requested: Path=" + _device);
+        }
+        
+    }
+}
---
+++ src/main/java/com/github/hypfvieh/sandbox/dbus/PulseAudioDbus.java
@@ -0,0 +1,49 @@
+package com.github.hypfvieh.sandbox.dbus;
+
+import java.io.File;
+
+import org.freedesktop.dbus.connections.impl.DBusConnection;
+import org.freedesktop.dbus.connections.impl.DBusConnection.DBusBusType;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.dbus.interfaces.Properties;
+
+public class PulseAudioDbus {
+
+    public static void main(String[] _args) throws DBusException {
+        DBusConnection sessionConnection = DBusConnection.getConnection(DBusBusType.SESSION);
+
+        Properties properties = sessionConnection.getRemoteObject("org.pulseaudio.Server", "/org/pulseaudio/server_lookup1", org.freedesktop.dbus.interfaces.Properties.class);
+        String address =  properties.Get("org.PulseAudio.ServerLookup1", "Address");
+
+        System.out.println("Found address: " + address + "\n");
+
+        sessionConnection.disconnect();
+
+        File file = new File(address.replace("unix:path=", ""));
+        if (!file.exists()) {
+            System.out.println("Extracted address " + address + " is not a valid file/unix-socket");
+        } else {
+        	DBusConnection connection = DBusConnection.getConnection(address, false, false);
+
+            Properties core1Props = connection.getRemoteObject("org.PulseAudio.Core1", "/org/pulseaudio/core1", org.freedesktop.dbus.interfaces.Properties.class);
+            System.out.println("PulseAudio Name: " + core1Props.Get("org.PulseAudio.Core1", "Name"));
+            System.out.println("PulseAudio Version: " + core1Props.Get("org.PulseAudio.Core1", "Version"));
+
+            System.out.println("-----------------------------------------");
+            System.out.println("PulseAudio Hostname: " + core1Props.Get("org.PulseAudio.Core1", "Hostname"));
+            System.out.println("PulseAudio Username: " + core1Props.Get("org.PulseAudio.Core1", "Username"));
+
+            connection.disconnect();
+        }
+
+    }
+
+    public static void main2(String[] args) throws DBusException {
+        DBusConnection sessionConnection = DBusConnection.getConnection(DBusBusType.SESSION);
+        org.freedesktop.dbus.interfaces.Properties properties = sessionConnection.getRemoteObject("org.pulseaudio.Server", "/org/pulseaudio/server_lookup1", org.freedesktop.dbus.interfaces.Properties.class);
+        String address = properties.Get("org.PulseAudio.ServerLookup1", "Address");
+        sessionConnection.disconnect();
+
+        System.out.println(address);
+    }
+}
